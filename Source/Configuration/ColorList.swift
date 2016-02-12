107de1eae719867504d724c23c5b44df4d1e7866
JP Wright <jpwright31@gmail.com>
2016-02-12 11:53:13 -0800

    use djz colors for whisper announcements

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -4,10 +4,10 @@ public struct ColorList {
 
   public struct Shout {
 
-    public static var background = UIColor.whiteColor()
+    public static var background = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
     public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
-    public static var title = UIColor.blackColor()
-    public static var subtitle = UIColor.blackColor()
+    public static var title = UIColor.whiteColor()
+    public static var subtitle = UIColor.whiteColor()
   }
 
   public struct Whistle {
